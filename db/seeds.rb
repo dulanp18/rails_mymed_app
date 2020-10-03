70fffc7593c77695986be2807e42624912071d4f
Nicholas Barkolias <nbarkolias@gmail.com>
2020-10-03 14:37:57 +1000

    seeds created

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,18 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-1st = User.new(first_name: 'Nicholas', last_name: 'Barkolias', date_of_birth: DateTime.strptime("18/02/1989", "%m/%d/%Y"), contact_number: '0411332270', password: 'password', nationality: 'Australian')
-1st.save
-Doctor.create(user: 1st, medical_licence_number: 00000, country_of_doctor_licence: 'Australia', clinic_of_practice: 'Melbourne', address_of_clinic_string: 'Collins street, Melbourne')
+Doctor.delete_all
+Patient.delete_all
+User.delete_all
+
+puts "deleted previous"
+
+first_doctor = User.new(first_name: 'Nicholas', last_name: 'Barkolias', date_of_birth: DateTime.new(2009,9,1,17), contact_number: '0411332270', password: 'password', nationality: 'Australian', email: 'nbarkolias@gmail.com', user_type: 'doctor')
+first_doctor.save!
+Doctor.create!(user: first_doctor, medical_licence_number: 00000, country_of_doctor_licence: 'Australia', clinic_of_practice: 'Melbourne', address_of_clinic_string: 'Collins street, Melbourne')
+
+first_patient = User.new(first_name: 'Nick', last_name: 'Barkolias', date_of_birth: DateTime.new(2009,9,1,17), contact_number: '0411332270', password: 'password', nationality: 'Australian', email: 'fake@gmail.com', user_type: 'patient')
+first_patient.save!
+Patient.create!(home_address: '17 milton road', allergies: 'none', user: first_patient)
+
+puts "seeded"
