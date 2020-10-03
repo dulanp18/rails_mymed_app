cb6c12894f332cc90178f8a3ce6b274b06453420
Dulan Perera <dulanp18@gmail.com>
2020-10-03 18:44:07 +1300

    added patient signup

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,15 +1,15 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
   include Pundit
-  after_action :verify_authorized, except: :index, unless: :skip_pundit?
-  after_action :verify_policy_scoped, only: :index, unless: :skip_pundit?
+  #after_action :verify_authorized, except: :index, unless: :skip_pundit?
+  #after_action :verify_policy_scoped, only: :index, unless: :skip_pundit?
 
-  before_action :configure_permitted_parameters, if: :devise_controller?
 
   def configure_permitted_parameters
     # For additional fields in app/views/devise/registrations/new.html.erb
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :date_of_birth, :nationality, :contact_number,:user_type])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :date_of_birth, :nationality, :contact_number,:user_type ])
 
     # For additional in app/views/devise/registrations/edit.html.erb
     devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name, :date_of_birth, :nationality, :contact_number])
@@ -25,9 +25,9 @@ protected
 
   def after_sign_in_path_for(resource)
     if current_user.user_type == 'doctor'
-      doctor_new_path
+      new_doctor_path
     elsif current_user.user_type == 'patient'
-      patient_new_path
+      new_patient_path
     end
   end
 end
--- app/models/patient.rb
+++ app/models/patient.rb
@@ -1,7 +1,7 @@
 class Patient < ApplicationRecord
   belongs_to :user
   has_many :consultations
-  has_many :prescriptions, through: consultations:
+  has_many :prescriptions, through: :consultations
   has_many :orders, through: :consultations
 
 
