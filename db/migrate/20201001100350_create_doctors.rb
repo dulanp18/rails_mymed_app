973bbe0aa881c932b829e051102cc6dbd13fead6
Nicholas Barkolias <nbarkolias@gmail.com>
2020-10-03 14:13:04 +1000

    migrations fixed

---
+++ db/migrate/20201001100350_create_doctors.rb
@@ -0,0 +1,15 @@
+class CreateDoctors < ActiveRecord::Migration[6.0]
+  def change
+  create_table "doctors", force: :cascade do |t|
+    t.integer "medical_licence_number", null: false
+    t.string "country_of_doctor_licence", null: false
+    t.string "clinic_of_practice", null: false
+    t.string "address_of_clinic_string", null: false
+    t.string "profile_status", default: "unapproved", null: false
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_doctors_on_user_id"
+  end
+  end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+1st = User.new(first_name: 'Nicholas', last_name: 'Barkolias', date_of_birth: DateTime.strptime("18/02/1989", "%m/%d/%Y"), contact_number: '0411332270', password: 'password', nationality: 'Australian')
+1st.save
+Doctor.create(user: 1st, medical_licence_number: 00000, country_of_doctor_licence: 'Australia', clinic_of_practice: 'Melbourne', address_of_clinic_string: 'Collins street, Melbourne')
