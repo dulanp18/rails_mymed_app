b02ff8ec9db4917c6387de4216928721dfaf3a9e
Luca Haar <luca.haar@icloud.com>
2020-10-20 19:47:03 +1100

    updated amounts must be greater or equal to '1' to work

--- app/models/prescription.rb
+++ app/models/prescription.rb
@@ -3,7 +3,7 @@ class Prescription < ApplicationRecord
   belongs_to :consultation
   has_many :task
   monetize :price_cents
-  validates :amount_per_serving, numericality: { greater_than_or_equal_to: 0 }
-  validates :servings_per_day, numericality: { greater_than_or_equal_to: 0 }
-  validates :number_of_days, numericality: { greater_than_or_equal_to: 0 }
+  validates :amount_per_serving, numericality: { greater_than_or_equal_to: 1 }
+  validates :servings_per_day, numericality: { greater_than_or_equal_to: 1 }
+  validates :number_of_days, numericality: { greater_than_or_equal_to: 1 }
 end
